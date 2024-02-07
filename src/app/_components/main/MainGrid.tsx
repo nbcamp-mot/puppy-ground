66b1e540a3d45543911aa0b2c51c841d0654093a
yeahzzl <yeahzzl1018@gmail.com>
2024-02-07 11:57:21 +0900

    chore: pull후 수정(#148)

--- src/app/_components/main/MainGrid.tsx
+++ src/app/_components/main/MainGrid.tsx
@@ -16,7 +16,6 @@ import { motion } from 'framer-motion';
 const MainGrid = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.header}></div>
       <div className={styles.firstContainer}>
         <motion.div
           className={styles.firstSection}
